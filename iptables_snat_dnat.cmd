3fc05810fd8675fedacf944b9a2458e77d4f8143
Shaoyaming <yaming.shao@agrant.cn>
2016-04-12 11:39:25 +0800

    20160412B

--- iptables_snat_dnat.cmd
+++ iptables_snat_dnat.cmd
@@ -3,3 +3,5 @@
 
 
 /sbin/iptables -t nat -A PREROUTING -s *.*.*.*/32 -i em2 -p tcp -m tcp --dport 59208 -j DNAT --to-destination *.*.*.*:14000
+
+/sbin/iptables -t nat -A POSTROUTING -s 10.0.0.1 -d 10.4/16 -j SNAT --to 10.4.1.166
